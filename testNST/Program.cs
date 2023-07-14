db914c89a76e9c9e5748b25ffc645ebdb2fb12ce
Sandak Balzhiev <Sandakella@yandex.ru>
2023-07-14 13:34:57 +0700

    удалил ненужное

--- testNST/Program.cs
+++ testNST/Program.cs
@@ -10,14 +10,11 @@ namespace testNST
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            
-            // Add services to the container.
-
             builder.Services.AddDbContext<PSContext>
-                (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")/*builder.Configuration["ConnectionString:DefaultConnection"]*/));
+                (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -25,7 +22,6 @@ namespace testNST
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
